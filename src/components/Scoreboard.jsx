91c1b540524530fbe8fa6c139678624f73f3d554
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-09-12 15:39:35 -0700

    feat: create states for current score and best score
    Use state to keep track and update scoreboard.

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import '../styles/Scoreboard.css'
 
 function Scoreboard() {
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+
   return (
     <section>
-      <p>Score: </p>
-      <p>Best Score: </p>
+      <p>Score: {score}</p>
+      <p>Best Score: {bestScore}</p>
     </section>
   )
 }
