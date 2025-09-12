75238241bb4b0d592dfce8dc9bf4c010899235cd
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-09-11 22:06:55 -0700

    feat: create section for scoreboard
    Use semantic HTML for sections in main content.

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,7 +1,12 @@
 import '../styles/Scoreboard.css'
 
 function Scoreboard() {
-
+  return (
+    <section>
+      <p>Score: </p>
+      <p>Best Score: </p>
+    </section>
+  )
 }
 
 export default Scoreboard
