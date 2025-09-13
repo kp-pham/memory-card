605a17d7cd8adb84327107156804a18e530ca381
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-09-12 17:02:46 -0700

    feat: create array of URLs for API requests
    Shuffle API requests to randomize images.

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,6 +1,13 @@
 import Card from './Card.jsx'
 import '../styles/Gameboard.css'
 
+const images = [
+  'https://genshin.jmp.blue/characters/neuvillette/gacha-card',
+  'https://genshin.jmp.blue/characters/furina/gacha-card',
+  'https://genshin.jmp.blue/characters/hu-tao/gacha-card',
+  'https://genshin.jmp.blue/characters/barbara/gacha-card',
+]
+
 function Gameboard() {
   return (
     <section>
