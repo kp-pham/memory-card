0db6da1d25f6faa009dc4886bb3ad7943f181aa2
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-09-12 17:21:05 -0700

    feat: create shuffle algorithm
    Shuffle URLs for API requests to randomize images.

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -16,4 +16,11 @@ function Gameboard() {
   );
 }
 
-export default Gameboard;
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; --i) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  } 
+}
+
+export default Gameboard
