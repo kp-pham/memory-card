a79b18f504c3f4987de03ebf08f745c6d0800e35
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-09-12 21:28:06 -0700

    refactor: lift state from gameboard
    Share state with scoreboard from common ancestor for single source of truth.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,58 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Gameboard, Scoreboard } from './components'
 import './App.css'
+
+const images = [
+  'https://genshin.jmp.blue/characters/neuvillette/gacha-card',
+  'https://genshin.jmp.blue/characters/furina/gacha-card',
+  'https://genshin.jmp.blue/characters/venti/gacha-card',
+  'https://genshin.jmp.blue/characters/jean/gacha-card',
+  'https://genshin.jmp.blue/characters/lisa/gacha-card',
+  'https://genshin.jmp.blue/characters/barbara/gacha-card',
+  'https://genshin.jmp.blue/characters/kaeya/gacha-card',
+  'https://genshin.jmp.blue/characters/diluc/gacha-card',
+  'https://genshin.jmp.blue/characters/ningguang/gacha-card',
+  'https://genshin.jmp.blue/characters/beidou/gacha-card',
+  'https://genshin.jmp.blue/characters/zhongli/gacha-card',
+  'https://genshin.jmp.blue/characters/hu-tao/gacha-card',
+  'https://genshin.jmp.blue/characters/xianyun/gacha-card',
+  'https://genshin.jmp.blue/characters/gaming/gacha-card',
+  'https://genshin.jmp.blue/characters/yanfei/gacha-card',
+  'https://genshin.jmp.blue/characters/raiden/gacha-card',
+  'https://genshin.jmp.blue/characters/yae-miko/gacha-card',
+  'https://genshin.jmp.blue/characters/sara/gacha-card',
+  'https://genshin.jmp.blue/characters/arataki-itto/gacha-card',
+  'https://genshin.jmp.blue/characters/ayaka/gacha-card',
+]
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [shuffled, setShuffled] = useState([])
+
+  useEffect(() => {
+    setShuffled(shuffle(images))
+  }, [])
+
+  const handleCardClick = () => setShuffled(shuffle(images))
 
   return (
     <>
       <Scoreboard score={score} bestScore={bestScore}/>
-      <Gameboard/>
+      <Gameboard shuffled={shuffled} handleCardClick={handleCardClick}/>
     </>
   )
+}
+
+function shuffle(array) {
+  const shuffled = [...array]
+
+  for (let i = array.length - 1; i > 0; --i) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled;
 }
 
 export default App
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,55 +1,13 @@
-import { useEffect, useState } from 'react'
 import Card from './Card.jsx'
 import '../styles/Gameboard.css'
 
-const images = [
-  'https://genshin.jmp.blue/characters/neuvillette/gacha-card',
-  'https://genshin.jmp.blue/characters/furina/gacha-card',
-  'https://genshin.jmp.blue/characters/venti/gacha-card',
-  'https://genshin.jmp.blue/characters/jean/gacha-card',
-  'https://genshin.jmp.blue/characters/lisa/gacha-card',
-  'https://genshin.jmp.blue/characters/barbara/gacha-card',
-  'https://genshin.jmp.blue/characters/kaeya/gacha-card',
-  'https://genshin.jmp.blue/characters/diluc/gacha-card',
-  'https://genshin.jmp.blue/characters/ningguang/gacha-card',
-  'https://genshin.jmp.blue/characters/beidou/gacha-card',
-  'https://genshin.jmp.blue/characters/zhongli/gacha-card',
-  'https://genshin.jmp.blue/characters/hu-tao/gacha-card',
-  'https://genshin.jmp.blue/characters/xianyun/gacha-card',
-  'https://genshin.jmp.blue/characters/gaming/gacha-card',
-  'https://genshin.jmp.blue/characters/yanfei/gacha-card',
-  'https://genshin.jmp.blue/characters/raiden/gacha-card',
-  'https://genshin.jmp.blue/characters/yae-miko/gacha-card',
-  'https://genshin.jmp.blue/characters/sara/gacha-card',
-  'https://genshin.jmp.blue/characters/arataki-itto/gacha-card',
-  'https://genshin.jmp.blue/characters/ayaka/gacha-card',
-]
-
-function Gameboard() {
-  const [shuffled, setShuffled] = useState([])
-
-    useEffect(() => {
-      setShuffled(shuffle(images))
-    }, [])
-
-  const reshuffle = () => setShuffled(shuffle(images))
+function Gameboard({ shuffled, handleCardClick }) {
 
   return (
     <section id="card-grid">
-        {shuffled.map(requestURL => <Card requestURL={requestURL} onClick={reshuffle} />)}
+        {shuffled.map(requestURL => <Card requestURL={requestURL} onClick={handleCardClick} />)}
     </section>
   );
-}
-
-function shuffle(array) {
-  const shuffled = [...array]
-
-  for (let i = array.length - 1; i > 0; --i) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-
-  return shuffled;
 }
 
 export default Gameboard
