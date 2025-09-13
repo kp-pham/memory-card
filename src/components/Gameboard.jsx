c43fad0378d5b7d12e430d354cdbdc59bb6cffb5
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-09-12 18:02:37 -0700

    refactor: create card components after shuffling
    Randomize images for cards.

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -5,8 +5,24 @@ import '../styles/Gameboard.css'
 const images = [
   'https://genshin.jmp.blue/characters/neuvillette/gacha-card',
   'https://genshin.jmp.blue/characters/furina/gacha-card',
-  'https://genshin.jmp.blue/characters/hu-tao/gacha-card',
+  'https://genshin.jmp.blue/characters/venti/gacha-card',
+  'https://genshin.jmp.blue/characters/jean/gacha-card',
+  'https://genshin.jmp.blue/characters/lisa/gacha-card',
   'https://genshin.jmp.blue/characters/barbara/gacha-card',
+  'https://genshin.jmp.blue/characters/kaeya/gacha-card',
+  'https://genshin.jmp.blue/characters/diluc/gacha-card',
+  'https://genshin.jmp.blue/characters/ningguang/gacha-card',
+  'https://genshin.jmp.blue/characters/beidou/gacha-card',
+  'https://genshin.jmp.blue/characters/zhongli/gacha-card',
+  'https://genshin.jmp.blue/characters/hu-tao/gacha-card',
+  'https://genshin.jmp.blue/characters/xianyun/gacha-card',
+  'https://genshin.jmp.blue/characters/gaming/gacha-card',
+  'https://genshin.jmp.blue/characters/yanfei/gacha-card',
+  'https://genshin.jmp.blue/characters/raiden/gacha-card',
+  'https://genshin.jmp.blue/characters/yae-miko/gacha-card',
+  'https://genshin.jmp.blue/characters/sara/gacha-card',
+  'https://genshin.jmp.blue/characters/arataki-itto/gacha-card',
+  'https://genshin.jmp.blue/characters/ayaka/gacha-card',
 ]
 
 function Gameboard() {
@@ -18,7 +34,7 @@ function Gameboard() {
 
   return (
     <section>
-        <Card requestURL={shuffled[0]}/>
+        {shuffled.map(requestURL => <Card requestURL={requestURL}/>)}
     </section>
   );
 }
